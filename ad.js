65718604a914b62b83af0fa100f2d21e37e9a1cb
vnad8 <maolaguo@proton.me>
2026-02-09 15:10:12 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -252,23 +252,23 @@
     image: "https://cdn.jsdelivr.net/gh/vnad8/style@main/click.webp",
     width: 300,
     height: 250,
-    link: "https://ccfao.com"
+    link: "https://www.mmoo.love/Account/Register?f=1551362"
   });
 
   
 window.AdPopup.initBanner({
     image: "https://pub-e658d2b69e604cdba2ef90be9d9f8e6f.r2.dev/mmoo.webp", 
-    link: "https://ccfao.com"
+    link: "https://www.mmoo.love/Account/Register?f=1551362"
   });
 
 window.AdPopup.initBanner({
     image: "https://pub-e658d2b69e604cdba2ef90be9d9f8e6f.r2.dev/mmoo.webp", 
-    link: "https://ccfao.com"
+    link: "https://www.mmoo.love/Account/Register?f=1551362"
   });
 
 window.AdPopup.initBanner({
     image: "https://pub-e658d2b69e604cdba2ef90be9d9f8e6f.r2.dev/mmoo.webp", 
-    link: "https://ccfao.com"
+    link: "https://www.mmoo.love/Account/Register?f=1551362"
   });
   
 
@@ -278,7 +278,7 @@ window.AdPopup.initBanner({
 
 
 function goToBaseHost() {
-  window.open("https://ccfao.com", "_blank", "noopener,noreferrer");
+  window.open("https://www.mmoo.love/Account/Register?f=1551362", "_blank", "noopener,noreferrer");
 }
 
 
@@ -291,6 +291,7 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
+
 
 
 
